fa0c4d4fa7c69495be734602082b83854e3050e8
Alec Zackarian <21zackarianA@flintridgeprep.org>
2019-10-07 13:42:33 -0700

    I have completed everything in assignment 1.1 and am about to start assignment 1.2.

--- TestScanner.java
+++ TestScanner.java
@@ -1,33 +1,15 @@
-
-/**
- * Write a description of class TestScanner here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.util.*;
 public class TestScanner
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class TestScanner
-     */
-    public TestScanner()
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
-    }
+Scanner  consoleInput= new Scanner(System.in);
+System.out.println("Please Type Your Favorite Number");
+int number = consoleInput.nextInt();
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
+    }
+    
+   
+
+
